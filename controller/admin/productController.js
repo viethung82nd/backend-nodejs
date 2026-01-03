1efbdd27206212bae8c4ec279e8338c72b1890e9
viethung82nd <viethungnguyen204@gmail.com>
2026-01-03 18:42:49 +0700

    tối ưu filter và search

--- controller/admin/productController.js
+++ controller/admin/productController.js
@@ -1,34 +1,7 @@
 const Products = require("../../models/productModel");
+const filterStatusHelper = require("../../helpers/filterStatus");
 
 module.exports.product = async (req, res) => {
-  const filterStatus = [
-    {
-      name: "All",
-      status: "",
-      class: "",
-    },
-    {
-      name: "Active",
-      status: "active",
-      class: "",
-    },
-    {
-      name: "Inactive",
-      status: "inactive",
-      class: "",
-    },
-  ];
-
-  if (req.query.status) {
-    const index = filterStatus.findIndex(
-      (item) => item.status == req.query.status
-    );
-    filterStatus[index].class = "active";
-  } else {
-    const index = filterStatus.findIndex((item) => item.status == "");
-    filterStatus[index].class = "active";
-  }
-
   const find = {
     deleted: false,
   };
@@ -49,7 +22,7 @@ module.exports.product = async (req, res) => {
   res.render("admin/pages/products/index", {
     title: "Products",
     products: products,
-    filterStatus: filterStatus,
+    filterStatus: filterStatusHelper(req.query),
     keyword: req.query.keyword,
   });
 };

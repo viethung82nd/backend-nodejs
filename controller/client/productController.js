16acf50f1d6ab49fc8f3f781b5b3fb5ab8db58c8
viethung82nd <viethungnguyen204@gmail.com>
2026-02-01 01:16:51 +0700

    fix deploy 2

--- controller/client/productController.js
+++ controller/client/productController.js
@@ -1,23 +1,28 @@
 const Product = require("../../models/productModel");
 
 module.exports.index = async (req, res) => {
-  const products = await Product.find({
-    status: "active",
-    deleted: false,
-  }).sort({ position: "desc" });
+  try {
+    const products = await Product.find({
+      status: "active",
+      deleted: false,
+    }).sort({ position: "desc" });
 
-  const newProducts = products.map((item) => {
-    item.priceNew = (
-      item.price -
-      (item.price * item.discountPercentage) / 100
-    ).toFixed(0);
-    return item;
-  });
+    const newProducts = products.map((item) => {
+      item.priceNew = (
+        item.price -
+        (item.price * item.discountPercentage) / 100
+      ).toFixed(0);
+      return item;
+    });
 
-  res.render("client/pages/products/index", {
-    title: "Products Page",
-    products: newProducts,
-  });
+    res.render("client/pages/products/index", {
+      title: "Products Page",
+      products: newProducts,
+    });
+  } catch (err) {
+    console.error("❌ Products error:", err);
+    res.status(500).send("Internal Server Error");
+  }
 };
 
 module.exports.detail = async (req, res) => {

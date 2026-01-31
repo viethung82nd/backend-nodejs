d401c73a32655814ef63cd4bfcbafa388ff5e453
viethung82nd <viethungnguyen204@gmail.com>
2026-02-01 01:07:39 +0700

    fix deploy

--- config/database.js
+++ config/database.js
@@ -2,9 +2,13 @@ const mongoose = require("mongoose");
 
 module.exports.connect = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URL);
-    console.log("Connected to MongoDB successfully");
+    await mongoose.connect(process.env.MONGODB_URL, {
+      bufferCommands: false, // 🔥 QUAN TRỌNG
+      serverSelectionTimeoutMS: 10000, // tránh treo vô hạn
+    });
+    console.log("✅ Connected to MongoDB successfully");
   } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
+    console.error("❌ Error connecting to MongoDB:", error);
+    throw error;
   }
 };

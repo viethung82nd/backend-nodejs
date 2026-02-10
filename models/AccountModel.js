5e3bd043d14b05b9466ca57704522a38e3107aaa
viethung82nd <viethungnguyen204@gmail.com>
2026-02-10 12:21:32 +0700

    account model

---
+++ models/AccountModel.js
@@ -0,0 +1,27 @@
+var mongoose = require("mongoose");
+const generate = require("../helpers/generate");
+
+const accountSchema = new mongoose.Schema(
+  {
+    fullName: String,
+    email: String,
+    password: String,
+    token: {
+      type: String,
+      default: generate.generateRandomString(20),
+    },
+    phone: String,
+    avatar: String,
+    roleId: String,
+    status: String,
+    deleted: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true },
+);
+
+const Account = mongoose.model("Account", accountSchema, "accounts");
+
+module.exports = Account;
